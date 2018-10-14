cb11a402448bb8484a6bdbbf125b4068e24a1bca
adambartkowiak <adambartkowiak@op.pl>
2018-10-14 21:46:07 +0200

    Refactor Add SVG Grid Animation

--- src/homeproject.kt
+++ src/homeproject.kt
@@ -1,19 +1,13 @@
-import org.khronos.webgl.WebGLRenderingContext
-import org.w3c.dom.HTMLCanvasElement
-import org.w3c.dom.svg.SVGPatternElement
 import kotlin.browser.*
 
+const val ZOOM_STEP = 1.015
+const val ZOOM_ANIMATION_SPEED = 5
+const val ZOOM_ANIMATION_STEPS = 20
+var zoom: Double = 20.0
+
 fun main(args: Array<String>) {
-    val message = "Hello JavaScript!"
-    println(message)
-
     setButtons()
-    modifyText()
-//    zoomUp()
-//    zoomDown()
-
-
-//    start()
+    updateZoomText()
 }
 
 
@@ -21,73 +15,62 @@ fun setButtons() {
     val zoomUp = document.getElementById("zoomPlus")
     zoomUp?.addEventListener("click", {
         zoomUp()
-        println("zoomUp: $zoom")
     })
 
 
     val zoomDown = document.getElementById("zoomMinus")
     zoomDown?.addEventListener("click", {
         zoomDown()
-        println("zoomMinus: $zoom")
     })
-
 }
 
-var zoom: Double = 0.0
 
 fun zoomDown() {
-    zoom -= 0.2
 
-    val svgCanvas = document.getElementById("svg_canvas")
-    svgCanvas?.setAttribute("transform", "scale(1.0)")
+    val interval = window.setInterval({
+        zoom /= ZOOM_STEP
+        updateSvgGrid()
+        updateZoomText()
+    }, ZOOM_ANIMATION_SPEED)
+
+    window.setTimeout({ window.clearInterval(interval) }, ZOOM_ANIMATION_SPEED * ZOOM_ANIMATION_STEPS)
 }
+
 
 fun zoomUp() {
-    zoom += 0.2
 
-    val svgCanvas = document.getElementById("svg_canvas")
-    svgCanvas?.setAttribute("transform", "scale(50)")
+    val interval = window.setInterval({
+        zoom *= ZOOM_STEP
+        updateSvgGrid()
+        updateZoomText()
+    }, ZOOM_ANIMATION_SPEED)
 
+    window.setTimeout({ window.clearInterval(interval) }, ZOOM_ANIMATION_SPEED * ZOOM_ANIMATION_STEPS)
 }
 
-private fun modifyText() {
+private fun updateSvgGrid() {
+    val smallGrid = document.getElementById("smallGrid")
+    smallGrid?.setAttribute("width", zoom.toString())
+    smallGrid?.setAttribute("height", zoom.toString())
+
+    val smallGridPath = document.getElementById("smallGridPath")
+    smallGridPath?.setAttribute("d", "M $zoom 0 L 0 0 0 $zoom")
+
+    val zoomExtra = zoom * 10
+    val grid = document.getElementById("grid")
+    grid?.setAttribute("width", zoomExtra.toString())
+    grid?.setAttribute("height", zoomExtra.toString())
+
+    val gridRect = document.getElementById("gridRect")
+    gridRect?.setAttribute("width", zoomExtra.toString())
+    gridRect?.setAttribute("height", zoomExtra.toString())
+
+
+    val gridPath = document.getElementById("gridPath")
+    gridPath?.setAttribute("d", "M $zoomExtra 0 L 0 0 0 $zoomExtra")
+}
+
+private fun updateZoomText() {
     val element = document.getElementById("text")
-    element?.innerHTML = "nowe cos"
+    element?.innerHTML = zoom.toString()
 }
-
-//var gl: WebGLRenderingContext? = null
-//fun start() {
-//    val canvas = document.getElementById("glcanvas") as HTMLCanvasElement
-//
-//    gl = initWebGL(canvas)
-//
-//    if (gl != null) {
-//        gl!!.clearColor(0.0F, 0.0F, 0.0F, 1.0F)
-//        gl!!.enable(WebGLRenderingContext.DEPTH_TEST)
-//        gl!!.depthFunc(WebGLRenderingContext.LEQUAL)
-//        gl!!.clear(WebGLRenderingContext.COLOR_BUFFER_BIT or WebGLRenderingContext.DEPTH_BUFFER_BIT)
-//    }
-//
-//    val vertices: FloatArray = floatArrayOf(
-//            -0.5F, 0.5F, 0.0F,
-//            -0.5F, -0.5F, 0.0F,
-//            0.5F, -0.5F, 0.0F
-//    )
-//
-//    val indices: IntArray = intArrayOf(0, 1, 2)
-//}
-//
-//fun initWebGL(canvas: HTMLCanvasElement?): WebGLRenderingContext? {
-//    gl = null
-//
-//    gl = canvas?.getContext("webgl") as WebGLRenderingContext?
-//    //canvas.getContext("experimental-webgl")
-//
-//    // If we don't have a GL context, give up now
-//    if (gl == null) {
-//        gl = null
-//        println("WebGL == null")
-//    }
-//
-//    return gl
-//}
